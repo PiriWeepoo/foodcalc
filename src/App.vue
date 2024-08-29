2384b7adf09bd4db22a3f861a9ce72e1d1102084
PiriWeepoo <egorilyenko@gmail.com>
2024-08-29 17:01:10 +0500

    fix 0.1

--- src/App.vue
+++ src/App.vue
@@ -133,12 +133,6 @@ provide('cartActions', {
 </script>
 
 <template>
-  <button
-    class="absolute bottom-2 right-2 h-12 w-12 rounded-full bg-indigo-200 text-4xl text-indigo-300 shadow-lg"
-    @click="openDrawer"
-  >
-    +
-  </button>
   <Drawer v-if="drawerOpen" />
   <div class="m-1 m-auto rounded-xl bg-white shadow-xl">
     <!-- <Header /> -->
@@ -170,6 +164,12 @@ provide('cartActions', {
     <div class="relative p-2">
       <CardList :items="items" @addToFavorite="addToFavorite" />
       <Accordion />
+      <button
+        class="absolute bottom-2 right-2 h-12 w-12 rounded-full bg-indigo-200 text-4xl text-indigo-300 shadow-lg"
+        @click="openDrawer"
+      >
+        +
+      </button>
     </div>
   </div>
 </template>
